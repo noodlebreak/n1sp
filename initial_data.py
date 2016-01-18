2aa5cfdbce0e2045430d15b04942c2f5f7cecfed
noodlebreak <ashish26kr91@gmail.com>
2016-01-18 22:25:48 +0530

    wrapped up in main(), so that it can be called from shell

--- initial_data.py
+++ initial_data.py
@@ -1,27 +1,33 @@
 # coding: utf-8
 
 from theapp.models import *
-usernames = ['jack', 'ginny', 'ralph']
-passwords = ['password'] * 5
-locations = ['Delhi', 'Noida', 'Bangalore']
-emails = ['jack@mail.com', 'ginny@mail.com',
-          'ralph@mail.com']
-for loc in locations:
-    City.objects.create(name=loc)
-cities = City.objects.all()
-for un, pwd, email, loc in zip(usernames, passwords, emails, cities):
-    user = User(username=un, location=loc, email=email)
-    user.set_password(pwd)
-    user.save()
-at1_group = Group.objects.create(name='AdminType1')
-at2_group = Group.objects.create(name='AdminType2')
 
-admin_L1 = User.objects.create_superuser(
-    username='admin_l1', email='l1@admin.com',
-    password='pass', location=City.objects.last())
-admin_L2 = User.objects.create_superuser(
-    username='admin_l2', email='gmail@superman.com',
-    password='pass', location=City.objects.last())
 
-admin_L1.groups.add(at1_group)
-admin_L2.groups.add(at2_group)
+def main():
+    usernames = ['jack', 'ginny', 'ralph']
+    passwords = ['password'] * 5
+    locations = ['Delhi', 'Noida', 'Bangalore']
+    emails = ['jack@mail.com', 'ginny@mail.com',
+              'ralph@mail.com']
+    for loc in locations:
+        City.objects.create(name=loc)
+    cities = City.objects.all()
+    for un, pwd, email, loc in zip(usernames, passwords, emails, cities):
+        user = User(username=un, location=loc, email=email)
+        user.set_password(pwd)
+        user.save()
+    at1_group = Group.objects.create(name='AdminType1')
+    at2_group = Group.objects.create(name='AdminType2')
+
+    admin_L1 = User.objects.create_superuser(
+        username='admin_l1', email='l1@admin.com',
+        password='pass', location=City.objects.last())
+    admin_L2 = User.objects.create_superuser(
+        username='admin_l2', email='gmail@superman.com',
+        password='pass', location=City.objects.last())
+
+    admin_L1.groups.add(at1_group)
+    admin_L2.groups.add(at2_group)
+
+if __name__ == '__main__':
+    main()
